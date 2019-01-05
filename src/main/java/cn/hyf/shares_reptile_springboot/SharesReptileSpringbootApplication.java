411d5ee84e02c0619e502c2737fba798e5c4abd3
feiyun123 <feiyun123@163.com>
2019-01-05 22:10:08 +0800

    first commit

---
+++ src/main/java/cn/hyf/shares_reptile_springboot/SharesReptileSpringbootApplication.java
@@ -0,0 +1,14 @@
+package cn.hyf.shares_reptile_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharesReptileSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharesReptileSpringbootApplication.class, args);
+	}
+
+}
+
